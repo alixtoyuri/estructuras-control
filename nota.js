5259be6601afdcdbf517a6fa681569e64b3893de
Alix <alixtoyuri@gmail.com>
2026-08-31 15:30:48 -0600

    Create nota.js

---
+++ nota.js
@@ -0,0 +1,9 @@
+/*
+3. Clasificador de calificación
+Escribe una función clasificar(nota) que reciba una nota de 0 a 100 y devuelva:
+
+90-100: "Excelente"
+70-89: "Aprobado"
+60-69: "Necesita mejorar"
+menos de 60: "Reprobado"
+*/
