b8847050cef351823d1b337c4d12f600fc3c2078
Alix <alixtoyuri@gmail.com>
2026-08-31 15:31:28 -0600

    Create dia-mes.js

---
+++ dia-mes.js
@@ -0,0 +1,5 @@
+/*
+4. Días del mes
+Escribe una función diasDelMes(mes) que reciba un número del 1 al 12 
+y devuelva cuántos días tiene ese mes (ignora años bisiestos por ahora, febrero = 28).
+*/
