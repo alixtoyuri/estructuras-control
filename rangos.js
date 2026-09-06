26b20bf54cf830e5e0bec35411c00b3a29b9e690
Alix <alixtoyuri@gmail.com>
2026-09-06 14:13:22 -0600

    Create rangos.js

---
+++ rangos.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 7 — Número entre rangos
+
+Pide un número y muestra:
+
+Si está entre 1 y 10:
+"Está entre 1 y 10"
+
+Si está entre 11 y 20:
+"Está entre 11 y 20"
+
+Si es mayor a 20:
+"Es mayor que 20"
+
+Si es menor a 1:
+"Es menor que 1"
+
+Parece sencillo, pero quiero que pienses:
+
+¿Estas condiciones son mutuamente excluyentes?
+*/
