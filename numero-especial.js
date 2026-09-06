662e140fa07a9def1ce8faf2674586d39ecfe215
Alix <alixtoyuri@gmail.com>
2026-09-06 13:42:38 -0600

    Update numero-especial.js

--- numero-especial.js
+++ numero-especial.js
@@ -8,3 +8,30 @@ Debes determinar:
 Si es positivo, negativo o cero.
 Si es par o impar.
 */
+
+let num = prompt ("Digita un numero:  ");
+let result= "";
+
+if(num >= 1){
+    result += " Positivo ";
+}
+if(num<=-1){
+    result += " Negativo ";
+}
+if(num=== 0){
+    result += " Cero ";
+}
+if(num % 2 === 0){
+    result += " Par ";
+}
+if(num %2 ===1){
+    result += " Impar ";
+}
+if (result ===""){
+    return String(result);
+}
+
+console.log(result);
+
+
+
