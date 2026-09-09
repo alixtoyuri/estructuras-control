af004e2d6b5f74fbf632b43bcab73f8419ed10fd
Alix <alixtoyuri@gmail.com>
2026-09-09 15:27:18 -0600

    Create sistema-clasificacion.js

---
+++ sistema-clasificacion.js
@@ -0,0 +1,31 @@
+/*
+Ejercicio 10 — Sistema de clasificación
+
+Pide al usuario:
+
+edad
+y:
+temperatura
+
+El programa debe determinar:
+
+Edad
+< 0       → Edad inválida
+0 - 12    → Niño
+13 - 17   → Adolescente
+18+       → Adulto
+Temperatura
+
+Independientemente de la edad:
+< 10       → Hace frío
+10 - 25    → Temperatura agradable
+> 25       → Hace calor
+
+Por ejemplo:
+Edad: 15
+Temperatura: 30
+
+Resultado:
+Adolescente
+Hace calor
+*/
