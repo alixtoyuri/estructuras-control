c39575ce8600fa6ff332d6334fe87c6d0b7a7668
Alix <alixtoyuri@gmail.com>
2026-09-09 16:16:15 -0600

    Update par-positivo-negativo.js

--- par-positivo-negativo.js
+++ par-positivo-negativo.js
@@ -19,3 +19,21 @@ if (resultado === ""){
 console.log(resultado);
 
 
+//Segundo Intento
+let number = Number( prompt("Un número es Positivo\nUn número es Negativo\nUn número es Cero\nUn número es Par\nDigite un número para determinarlo: "));
+let resultado = "";
+
+if (number > 0){
+    resultado += " El número es positivo ";
+}
+ else if (number < 0){
+    resultado += " El número es negativo ";
+}
+else {
+    resultado += " El número es cero ";
+}
+if(number % 2 === 0){
+    resultado += " El número es par ";
+}
+
+console.log(resultado);
