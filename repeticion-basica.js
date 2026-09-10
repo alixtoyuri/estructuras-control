abe59b68aa78e95e46afde77bb70cf736a63185b
Alix <alixtoyuri@gmail.com>
2026-09-10 16:18:41 -0600

    Create repeticion-basica.js

---
+++ repeticion-basica.js
@@ -0,0 +1,14 @@
+/*
+Crea un programa que utilice un for para mostrar en consola:
+
+Hola
+Hola
+Hola
+Hola
+Hola
+Condiciones
+
+Debes utilizar un for.
+
+El mensaje debe aparecer exactamente 5 veces.
+*/
