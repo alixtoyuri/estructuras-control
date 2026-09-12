06f0da0311c96ad8a938e6b12e2d09cc0078bb7a
Alix <alixtoyuri@gmail.com>
2026-09-12 16:03:45 -0600

    Create num-par.js

---
+++ num-par.js
@@ -0,0 +1,22 @@
+/*
+Ejercicio 4 — Números pares
+Muestra los números pares del 2 al 20.
+
+Resultado:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/
+
+for (let i= 0; i <= 20; i++){
+    if(i%2=== 0){
+        console.log(i);
+    }
+}
