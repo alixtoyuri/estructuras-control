e94b7a0e7b0438874110242b9799fefe05bbfaf3
Alix <alixtoyuri@gmail.com>
2026-09-12 15:58:45 -0600

    Create diez-uno.js

---
+++ diez-uno.js
@@ -0,0 +1,21 @@
+/*
+Contar hacia atrás
+Muestra los números desde 10 hasta 1.
+
+El resultado esperado:
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
+
+for (let i= 10; i >= 1; i--){
+    console.log(i);
+}
