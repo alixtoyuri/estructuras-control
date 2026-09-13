c3b491043e062105146ee64a551363ecb9089c18
Alix <alixtoyuri@gmail.com>
2026-09-13 15:41:34 -0600

    Create multiplos-cinco.js

---
+++ multiplos-cinco.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 5 — Múltiplos de 5
+Muestra los múltiplos de 5 desde 5 hasta 50.
+Resultado:
+
+5
+10
+15
+20
+25
+30
+35
+40
+45
+50
+*/
+for(let i = 1; i<= 50; i++){
+    if(i%5 === 0){
+        console.log(i);
+    }
+}
