627e34442de984c94de0c510f48be5f08f34a601
Alix <alixtoyuri@gmail.com>
2026-09-13 15:50:09 -0600

    Create suma-uno-diez.js

---
+++ suma-uno-diez.js
@@ -0,0 +1,16 @@
+/*
+Ejercicio 7 — Sumar del 1 al 10
+
+Utiliza un for para calcular:
+1 + 2 + 3 + ... + 10
+
+El programa debe mostrar:
+55
+*/
+let resultado = 0;
+
+for( let i= 1; i<=10; i++){
+    resultado += i;
+}
+
+console.log(resultado);
