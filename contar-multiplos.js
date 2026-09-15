da68d9ed2b814814d08d35c281569cf4f9cfd7a3
Alix <alixtoyuri@gmail.com>
2026-09-15 17:24:07 -0600

    Create contar-multiplos.js

---
+++ contar-multiplos.js
@@ -0,0 +1,19 @@
+/* Ejercicio 10 — Contar múltiplos
+
+Pide al usuario:
+Un número máximo
+Un divisor
+
+Después recorre desde 1 hasta el número máximo.
+Debes contar cuántos números son divisibles entre el divisor.
+
+Por ejemplo:
+Máximo: 20
+Divisor: 3
+
+Los números encontrados serían:
+3, 6, 9, 12, 15, 18
+
+Resultado:
+Hay 6 múltiplos.
+*/
