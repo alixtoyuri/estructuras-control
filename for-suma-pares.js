10ed07376643d7824e30624b33a040021195c4a6
Alix <alixtoyuri@gmail.com>
2026-09-17 16:09:11 -0600

    Create for-suma-pares.js

---
+++ for-suma-pares.js
@@ -0,0 +1,11 @@
+/*
+Ejercicio 12 — Suma de pares
+Recorre desde 1 hasta 100.
+Debes sumar solamente los números pares.
+Al finalizar muestra el resultado.
+
+La idea es practicar este patrón:
+recorrer
+→ comprobar
+→ acumular
+*/
