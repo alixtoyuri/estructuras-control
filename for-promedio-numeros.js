f9fcf35ed74e31f838d41a27cf9d9c0225284072
Alix <alixtoyuri@gmail.com>
2026-09-18 17:26:22 -0600

    Create for-promedio-numeros.js

---
+++ for-promedio-numeros.js
@@ -0,0 +1,22 @@
+/*
+Ejercicio 16 — Promedio de números
+
+Pide al usuario cuántos números desea introducir.
+Por ejemplo:
+¿Cuántos números deseas ingresar? 5
+
+Después utiliza un for para solicitar esos 5 números.
+
+Al terminar:
+suma todos los números;
+calcula el promedio;
+muestra el resultado.
+
+Ejemplo
+Entrada:
+10,20,30,40,50
+
+Resultado:
+Suma: 150
+Promedio: 30
+*/
