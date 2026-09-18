90b52d955c9f6686aafede42c6bbb5d342ea3dc9
Alix <alixtoyuri@gmail.com>
2026-09-17 18:01:15 -0600

    Create for-encontrar-mayor.js

---
+++ for-encontrar-mayor.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 14 — Encontrar el número mayor
+Pide al usuario 5 números, uno por uno.
+Después determina cuál de ellos es el mayor.
+
+Por ejemplo:
+8, 15, 3, 21, 10
+
+Resultado: El número mayor es 21.
+*/
