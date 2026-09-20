fa2e721ea8c4cff97411a549faa64dd6d995168f
Alix <alixtoyuri@gmail.com>
2026-09-20 17:31:06 -0600

    Create for-es-primo.js

---
+++ for-es-primo.js
@@ -0,0 +1,18 @@
+/*
+Ejercicio 18 — ¿Es primo?
+
+Pide al usuario un número entero positivo.
+Determina si es primo.
+
+Recuerda:
+Un número primo solamente puede dividirse exactamente entre:
+1
+y
+él mismo
+
+Por ejemplo:
+7 → primo
+8 → no primo
+13 → primo
+15 → no primo
+*/
