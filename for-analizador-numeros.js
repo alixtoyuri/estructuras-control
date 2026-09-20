8c1503ab91614fec9aef2654537317824d46cba8
Alix <alixtoyuri@gmail.com>
2026-09-20 17:34:22 -0600

    Create for-analizador-numeros.js

---
+++ for-analizador-numeros.js
@@ -0,0 +1,43 @@
+/*
+Ejercicio 20 — Analizador de números
+
+Este será tu ejercicio integrador.
+Pide al usuario cuántos números desea introducir.
+Después utiliza un for para solicitar todos los números.
+
+Al terminar, el programa debe indicar:
+Cantidad de números ingresados
+Cantidad de positivos
+Cantidad de negativos
+Cantidad de ceros
+Cantidad de pares
+Cantidad de impares
+Número mayor
+Número menor
+Suma total
+Promedio
+
+Por ejemplo, si el usuario introduce:
+5
+-3
+8
+0
+12
+-5
+
+el programa debería obtener aproximadamente:
+Cantidad de números: 5
+Positivos: 2
+Negativos: 2
+Ceros: 1
+Pares: 3
+Impares: 2
+Mayor: 12
+Menor: -5
+Suma: 12
+Promedio: 2.4
+
+Regla importante
+Intenta resolverlo utilizando un solo for principal para recorrer los números.
+Puedes utilizar varios if dentro de él.
+*/
