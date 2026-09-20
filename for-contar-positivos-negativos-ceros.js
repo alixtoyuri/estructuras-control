8f944a1064a3a82f274f0b14dc9b6e0719e10388
Alix <alixtoyuri@gmail.com>
2026-09-20 17:30:08 -0600

    Create for-contar-positivos-negativos-ceros.js

---
+++ for-contar-positivos-negativos-ceros.js
@@ -0,0 +1,16 @@
+/*
+Ejercicio 17 — Contar positivos, negativos y ceros
+
+Pide al usuario cuántos números desea introducir.
+Después solicita esos números mediante un for.
+
+Debes mantener tres contadores diferentes:
+positivos
+negativos
+ceros
+
+Al finalizar muestra algo parecido a:
+Positivos: 4
+Negativos: 2
+Ceros: 1
+*/
