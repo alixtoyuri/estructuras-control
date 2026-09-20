51a1591f151b3add9cacb1596422ddb38b459e18
Alix <alixtoyuri@gmail.com>
2026-09-20 17:32:31 -0600

    Create for-buscar-numero.js

---
+++ for-buscar-numero.js
@@ -0,0 +1,22 @@
+/*
+Ejercicio 19 — Buscar un número
+
+Crea un programa que tenga una colección de números:
+[4, 8, 15, 16, 23, 42]
+Pide al usuario un número.
+Utiliza un for para recorrer el arreglo y determinar si el número existe.
+
+Por ejemplo:
+Número a buscar: 15
+Resultado: El número sí existe.
+
+Si introduce:
+20
+
+Resultado:
+El número no existe.
+
+Objetivo
+Aquí el for empieza a utilizarse como una herramienta para:
+recorrer información y buscar algo dentro de ella.
+*/
