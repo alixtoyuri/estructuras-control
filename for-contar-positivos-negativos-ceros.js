8be0534d1d8ae28ba9802b748ae7320ed515cde9
Alix <alixtoyuri@gmail.com>
2026-09-21 14:18:59 -0600

    Update for-contar-positivos-negativos-ceros.js

--- for-contar-positivos-negativos-ceros.js
+++ for-contar-positivos-negativos-ceros.js
@@ -14,3 +14,22 @@ Positivos: 4
 Negativos: 2
 Ceros: 1
 */
+let numeros = Number (prompt("¿Cuántos números? "));
+let positivos = 0;
+let negativos = 0;
+let ceros = 0;
+
+for(let i = 1; i<=numeros;i++){
+  let numero = Number (prompt("Número "+i+": "));
+  if(numero < 0){
+    negativos ++;
+  } else if(numero > 0){
+    positivos++;
+  } else{
+    ceros++;
+  }
+}
+
+console.log("Positivos: "+positivos);
+console.log("Negativos: "+negativos);
+console.log("Ceros: "+ceros);
