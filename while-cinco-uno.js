6a0e47de332d08d397f477b2e380f9b128cb0665
Alix <alixtoyuri@gmail.com>
2026-09-24 16:40:08 -0600

    Create while-cinco-uno.js

---
+++ while-cinco-uno.js
@@ -0,0 +1,22 @@
+/*
+Ejercicio 2 — Cuenta regresiva
+
+Muestra:
+5
+4
+3
+2
+1
+Objetivo
+Practicar un while donde la variable disminuye en lugar de aumentar.
+
+Piensa:
+“¿Qué debe cambiar en cada iteración para acercarme al final?”
+*/
+
+let i = 5;
+
+while(i>=1){
+    console.log(i);
+    i--;
+}
