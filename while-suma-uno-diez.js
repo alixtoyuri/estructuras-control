2e79a138625896f3d0248c7f60b446a2d990ebd4
Alix <alixtoyuri@gmail.com>
2026-09-25 20:09:41 -0600

    Update while-suma-uno-diez.js

--- while-suma-uno-diez.js
+++ while-suma-uno-diez.js
@@ -6,3 +6,30 @@ Calcula:
 
 Resultado: 55
 */
+/*
+Ejercicio 3 — Números pares
+
+Utiliza while para mostrar los números pares del 2 al 20.
+
+Resultado:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/
+
+let i = 1;
+let suma = 0;
+
+while(i<=10){
+    suma +=i;
+    i++;
+}
+
+console.log(suma);
