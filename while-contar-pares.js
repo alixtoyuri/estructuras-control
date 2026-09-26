d757c1be84e812bd7d389b067e9b352132df10fd
Alix <alixtoyuri@gmail.com>
2026-09-25 20:02:14 -0600

    Create while-contar-pares.js

---
+++ while-contar-pares.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 6 — Contar números pares
+
+Recorre los números del 1 al 20 utilizando while.
+Cada vez que encuentres un número par, aumenta un contador.
+
+Al finalizar:
+Cantidad de pares: 10
+*/
