8cb7b61a57dd0700579c425b2f45ad831bb1a1d5
Alix <alixtoyuri@gmail.com>
2026-09-25 20:00:44 -0600

    Create while-mostrar-multiplos.js

---
+++ while-mostrar-multiplos.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 5 — Mostrar múltiplos
+
+Pide al usuario un número.
+Después utiliza un while para mostrar sus primeros 10 múltiplos.
+
+Ejemplo:
+Número: 7
+
+Resultado:
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+*/
